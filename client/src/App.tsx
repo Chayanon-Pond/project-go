3908df7d413ffac2a946c3af778cb94164ed739f
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-07-22 19:05:09 +0700

    check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,9 @@ export const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:4000/api"
     : "https://project-go-production-a31a.up.railway.app/api";
+
+console.log("Current mode:", import.meta.env.MODE);
+console.log("Base URL:", BASE_URL);
 function App() {
   return (
     <div className="min-h-screen bg-base-200 ">
